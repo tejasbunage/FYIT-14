94109d2659dc5140c2944f2c36d73a1e631a1112
student <tejasbunage12345@gmail.com>
2018-09-16 11:09:11 +0530

    added test.sh filr

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "makinf foss folder"
+mkdir foss
+
+echo "going to foss folder"
+cd foss
+
+echo "creating text filr.txt"
+touch test file.txt
+
+echo ""
+echo "showing output of ls:"
+ls
+
+echo""
+echo"updating linux redos"
